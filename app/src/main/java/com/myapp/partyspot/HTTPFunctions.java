1a407c0ea68ea9816bc965e522dd36c4abf4b08c
cwallac <christopher.wallace@students.olin.edu>
2014-10-06 20:10:25 -0400

    Progress on seqarch so far

--- app/src/main/java/com/myapp/partyspot/HTTPFunctions.java
+++ app/src/main/java/com/myapp/partyspot/HTTPFunctions.java
@@ -9,7 +9,12 @@ import com.android.volley.toolbox.Volley;
 import android.content.Context;
 import android.util.Log;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 /**
  * Created by cwallace on 10/3/14.
@@ -18,13 +23,20 @@ public class HTTPFunctions {
 
     public Context context;
     public RequestQueue queue;
+    public ArrayList<SpotifyTrack> queriedTracks;
+    public int flag;
 
     public HTTPFunctions(Context context) {
         this.context = context;
         this.queue = Volley.newRequestQueue(context);
+        this.queriedTracks = new ArrayList<SpotifyTrack>();
+        this.flag = 0;
     }//ALWAYS PASS IN getActivity
 
+   
+
     public void get(String URL) { //WONT ALWAYS BE VOID, RETURN INFO FROM DATA
+        int flag = 0;
         JsonObjectRequest getRequest = new JsonObjectRequest(Request.Method.GET, URL, null,
                 new Response.Listener<JSONObject>()
                 {
@@ -32,6 +44,11 @@ public class HTTPFunctions {
                     public void onResponse(JSONObject response) {
                         // display response
                         Log.d("Response", response.toString());
+                        //JANK FIX SHOULD FIGURE OUT A BETTER WAY
+                        ArrayList<SpotifyTrack> results = JSONtoSpotifyTrack(response);
+                        Log("MADE","THIS IS PROGRESS")
+                            //JSONtoSpotifyTrack(response);
+
                     }
                 },
                 new Response.ErrorListener() {
@@ -44,6 +61,38 @@ public class HTTPFunctions {
 
 // add it to the RequestQueue
         queue.add(getRequest);
+
+    }
+
+    public ArrayList<SpotifyTrack> JSONtoSpotifyTrack(JSONObject object){
+            ArrayList<SpotifyTrack> returnArray = new ArrayList<SpotifyTrack>();
+        try {
+
+            JSONArray tracks = object.getJSONArray("tracks");
+            for (int i = 0; i <10; i++) {
+                JSONObject firstSong = tracks.getJSONObject(i);
+                JSONArray artist = firstSong.getJSONArray("artists");
+                String trackName = firstSong.getString("name");
+
+                String artistName = artist.getJSONObject(0).getString("name");
+                String artistHref = artist.getJSONObject(0).getString("href");
+                String href = firstSong.getString("href");
+                Log.d("Artsit", artist.toString());
+                Log.d("Artsit", trackName);
+                Log.d("Artsit", href);
+                Log.d("Artist", artistHref);
+                Log.d("Artist", artistName);
+                returnArray.add(new SpotifyTrack(artistHref, artistName, href, trackName));
+            }
+
+
+
+            //Log.d("TEST",res.getJSONObject(0));
+        }
+        catch (JSONException e) {
+            Log.d("JSONEXCEPTION",e.toString());
+        }
+        return returnArray;
     }
 
 }
