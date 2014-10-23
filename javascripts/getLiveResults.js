92a3a2de25543351409375a71f5b455e73ac0ea2
griffint <griffin.tschurwald@students.olin.edu>
2014-10-23 19:32:51 -0400

    delete autocomplete

---
+++ javascripts/getLiveResults.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    var availableTags = [
+      "Livin on a prayer",
+      "The penis song",
+      "java - macklemore",
+      "latch"
+      
+    ];
+    $( "#songSearch" ).autocomplete({
+      source: function
+    });
+  });
