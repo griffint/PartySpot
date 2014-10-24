0ba532b72427974c2de9dbe04bde0d5671ac3714
Sawyer Vaughan <sawyer.vaughan@students.olin.edu>
2014-10-23 20:31:03 -0400

    added parseJSON

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,9 +1,5 @@
 $(document).ready(function() {
 
-	function getJSON() {
-
-	}
-
 	function parseJSON(response) {
 		outputArtistList = []
 		outputAlbumList = []
@@ -17,14 +13,15 @@ $(document).ready(function() {
 			tempList = [];
 			tempList.push(albumJSON.items[i].name);
 			tempList.push(albumJSON.items[i].images[1].url);
+			tempList.push(albumJSON.items[i].external_urls.spotify);
 			outputAlbumList.push(tempList);
 		}
 
 		for (i=0;i<artistJSON.length;i++) {
 			tempList = [];
 			tempList.push(artistJSON.items[i].name);
-			tempList.push(artistJSON.items[i].images[2]);
-			tempList.push(artistJSON.items[i].images[2]);
+			tempList.push(albumJSON.items[i].images[1].url);
+			tempList.push(artistJSON.items[i].external_urls.spotify);
 			outputArtistList.push(tempList);
 		}
 
@@ -33,6 +30,7 @@ $(document).ready(function() {
 			tempList.push(trackJSON.items[i].name);
 			tempList.push(trackJSON.items[i].artists.name);
 			tempList.push(trackJSON.items[i].album.images[1].url);
+			tempList.push(albumJSON.items[i].external_urls.spotify);
 			outputTrackList.push(tempList);
 		}
 
@@ -42,4 +40,6 @@ $(document).ready(function() {
 	function display(response) {
 		
 	}
+
+	$("#spotifySearch").html('<iframe src="https://embed.spotify.com/?uri=spotify:track:4th1RQAelzqgY7wL53UGQt" width='+$(window).width()+' height='+$(window).height()+' frameborder="0" allowtransparency="true"></iframe>');
 })
