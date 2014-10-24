e774fba53b8b207cb19065df3584cf9350974f0a
cwallac <christopher.wallace@students.olin.edu>
2014-10-23 20:24:07 -0400

    GetJSON data is functioning, runs on document ready as of now

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,11 +1,31 @@
 $(document).ready(function() {
-
-	function getJSON() {
+	console.log("TESTING");
+	getJSONData("WIZ Khalifa","Spotify",15,true)
+	var currentObj = null;
+	function getJSONData(searchText,apiUse,limit,liveResults)  {
+		if (currentObj != null){
+			currentObj.abort() //Kill all previous running queries
+		}
+		searchText = searchText.replace(' ', '%20');
+		switch (apiUse){
+			case "Spotify":
+			 	var url = "https://api.spotify.com/v1/search?q=" + searchText + "&type=artist,track,album&limit=" + limit;
+			 	break;
+			case "Deezer":
+				//THIS IS WHERE WE WOULD DETERMINE THE URL FOR THE DEEZER API
+				break;
+		}
+		
+		currentObj = $.getJSON(url,function(data) {
+			parseJSON(data, liveResults)
+		});
+		
 
 	}
 
-	function parseJSON(response) {
-		
+
+	function parseJSON(response, liveResults) {
+		console.log(response,liveResults);
 	}
 
 	function display(response) {
